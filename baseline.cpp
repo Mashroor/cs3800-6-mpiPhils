7cee8cfe318488c886e2d4e103a74216609a7f64
Joshua Pondrom <japrkf@mst.edu>
2019-04-18 22:07:50 -0500

    initial idea

--- baseline.cpp
+++ baseline.cpp
@@ -16,6 +16,8 @@ const int MAXMESSAGES = 10;
 //if you change this base, update the Makefile "clean" accordingly
 const string fileBase = "outFile"; 
 
+char READY = true;
+
 int main ( int argc, char *argv[] ) 
 {
   int id; //my MPI ID
@@ -44,9 +46,21 @@ int main ( int argc, char *argv[] )
   int numWritten = 0;
   
   //setup message storage locations
-  int msgIn, msgOut;
-  int leftNeighbor = (id + p - 1) % p;
+//   int leftNeighbor = (id + p - 1) % p;
   int rightNeighbor = (id + 1) % p;
+  
+  std::string title;
+  bool oddAmount = p % 2 != 0;
+  if(p - 1 == id && oddAmount) {
+      title = "last";
+  } else if ( id % 2 == 0 ) {
+      title = "even";
+  } else {
+      title = "odd";
+  }
+
+
+
 
   pomerize P;
 
@@ -54,47 +68,69 @@ int main ( int argc, char *argv[] )
   string rFile = fileBase + to_string(rightNeighbor);
   ofstream foutLeft(lFile.c_str(), ios::out | ios::app );
   ofstream foutRight(rFile.c_str(), ios::out | ios::app );
+  bool firstIter = true;
 
   while (numWritten < MAXMESSAGES) {
-    //send 1 test message to each neighbor
-    	msgOut = rand() % p; //pick a number/message
-	MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, leftNeighbor, tag ); 
-    	msgOut = rand() % p; //pick a new number/message
-	MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, rightNeighbor, tag ); 
+    // msgOut = rand() % p; //pick a number/message
+	// MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, leftNeighbor, tag ); 
+    // msgOut = rand() % p; //pick a new number/message
+	// MPI::COMM_WORLD.Send ( &msgOut, 1, MPI::INT, rightNeighbor, tag ); 
         
-    //receive 1 test message from each neighbor
-	MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
-//	std::cout << "ID " << id << " receiving message " << msgIn << " from Philosopher ";
-//	std::cout << status.Get_source() << std::endl;
+	// MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
+	// MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
 
-	MPI::COMM_WORLD.Recv ( &msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status );
-//	std::cout << "ID " << id << " receiving message " << msgIn << " from Philosopher ";
-//	std::cout << status.Get_source() << std::endl;	
+    // Recieving
+    if(!firstIter && title == "even"){
+        MPI::COMM_WORLD.Recv( &READY, 1, MPI::BYTE, (p - 1), tag, status );
+    } else if ( title == "odd") {
+        MPI::COMM_WORLD.Recv( &READY, 1, MPI::BYTE, (id - 1), tag, status );
+    } else if (title == "last") {
+        for(int i = 1 ; i < p - 1; i += 2){
+            MPI::COMM_WORLD.Recv( &READY, 1, MPI::BYTE, i, tag, status );
+        }
+    }
 
-	//LET'S JUST IGNORE THE MESSAGES AND ASSUME IT'S SAFE TO WRITE TO THE FILE!
-    //std::cout << "ID: " << id << " CARELESSLY writing to " << lFile << " and " << rFile << endl;
-    //If you want to see correct poems, change MAXMESSAGES to something VERY small and add this sleep
-//	sleep(id); //will delay each process so the initial interleaving(s) will likely be OK
-  
-    //construct poem & output stanzas into the files 'simultaneously'
-	//we do this with an intermediate variable so both files contain the same poem!
-	foutLeft << id << "'s poem:" << endl;
-	foutRight << id << "'s poem:" << endl;
-	
-	string stanza1, stanza2, stanza3;
+    // if(title == "last") {
+    //     std::cout << "LAST IN THIS MF" << std::endl;
+    // } else if (title == "even") {
+    //     std::cout << "EVEN AF" << std::endl;
+    // } else {
+    //     std::cout << "ODD BICH" << std::endl;
+    // }
+
+
+    // Writing blocc
+    foutLeft << id << "'s poem:" << endl;
+    foutRight << id << "'s poem:" << endl;
+    
+    string stanza1, stanza2, stanza3;
     stanza1 = P.getLine();
-	foutLeft << stanza1 << endl;
+    foutLeft << stanza1 << endl;
     foutRight << stanza1 << endl;
 
-	stanza2 = P.getLine();
-	foutLeft << stanza2 << endl;
+    stanza2 = P.getLine();
+    foutLeft << stanza2 << endl;
     foutRight << stanza2 << endl;
 
-	stanza3 = P.getLine();
-	foutLeft << stanza3 << endl << endl;
+    stanza3 = P.getLine();
+    foutLeft << stanza3 << endl << endl;
     foutRight << stanza3 << endl << endl;
 
     numWritten++;
+    // Writing blocc over
+
+
+    if(title == "even"){
+        MPI::COMM_WORLD.Send( &READY, 1, MPI::BYTE, (id + 1) % p, tag );
+    } else if ( title == "odd" ) {
+        MPI::COMM_WORLD.Send( &READY, 1, MPI::BYTE, (p - 1) , tag );
+    } else if (title == "last" ) {
+        for(int i = 0; i < p - 1; i *= 2) {
+            MPI::COMM_WORLD.Send( &READY, 1, MPI::BYTE, i , tag );
+        }
+    }
+
+    firstIter = false;
   }
 
   foutLeft.close();
